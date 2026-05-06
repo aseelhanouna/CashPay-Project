6dca15f3a48c854352d2479039c287d34d1569de
aseelhanouna <147027361+aseelhanouna@users.noreply.github.com>
2026-05-06 12:17:33 +0300

    Update database_helper.dart

--- lib/data/database_helper.dart
+++ lib/data/database_helper.dart
@@ -16,38 +16,30 @@ final _secureStorage = const FlutterSecureStorage();
 
 DatabaseHelper._init();
 
-// Gett
 Future<Database> get database async {
 if (_database != null) return _database!;
-_database = await _initDB('cashpay.db'); // استدعاء الدالة الموحدة
+_database = await _initDB('cashpay.db');
 return _database!;
 }
 
 Future<Database> _initDB(String filePath) async {
 final dbPath = await getDatabasesPath();
 final path = join(dbPath, filePath);
-
-String? key = await _secureStorage.read(key: 'db_key');  
-
-if (key == null) {  
-  key = base64Url.encode(  
-    List<int>.generate(32, (_) => Random.secure().nextInt(256)),  
-  );  
-  await _secureStorage.write(key: 'db_key', value: key);  
-}  
-
-return await openDatabase(  
-  path,  
-  version: 1,  
-  password: key,  
-  onCreate: _createDB,  
+String? key = await _secureStorage.read(key: 'db_key');
+if (key == null) {
+  key = base64Url.encode(
+    List<int>.generate(32, (_) => Random.secure().nextInt(256)),
+  );
+  await _secureStorage.write(key: 'db_key', value: key);
+}
+return await openDatabase(
+  path,
+  version: 1,
+  password: key,
+  onCreate: _createDB,
 );
-
 }
 
-// ======================
-// CREATE TABLES
-// ======================
 Future _createDB(Database db, int version) async {
 await db.execute('''
 CREATE TABLE users (
@@ -62,56 +54,43 @@ balance REAL DEFAULT 100.0,
 sync_status TEXT DEFAULT 'pending'
 )
 ''');
-
-await db.execute('''  
-  CREATE TABLE transactions (  
-    id INTEGER PRIMARY KEY AUTOINCREMENT,  
-    tx_id TEXT UNIQUE NOT NULL,  
-    sender_id INTEGER NOT NULL,  
-    receiver_id INTEGER NOT NULL,  
-    amount REAL NOT NULL,  
-    type TEXT NOT NULL,  
-    status TEXT NOT NULL,  
-    signature TEXT NOT NULL,  
-    used INTEGER DEFAULT 0,  
-    created_at INTEGER NOT NULL,  
-    sync_status TEXT DEFAULT 'pending',  
-    timestamp TEXT,  
-    synced_at INTEGER  
-      
-  )  
-''');  
-await db.execute('''  
-CREATE TABLE fraud_logs (  
-  id INTEGER PRIMARY KEY AUTOINCREMENT,  
-  tx_id TEXT,  
-  reason TEXT,  
-  created_at INTEGER  
-)  
+await db.execute('''
+CREATE TABLE transactions (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  tx_id TEXT UNIQUE NOT NULL,
+  sender_id INTEGER NOT NULL,
+  receiver_id INTEGER NOT NULL,
+  amount REAL NOT NULL,
+  type TEXT NOT NULL,
+  status TEXT NOT NULL,
+  signature TEXT NOT NULL,
+  used INTEGER DEFAULT 0,
+  created_at INTEGER NOT NULL,
+  sync_status TEXT DEFAULT 'pending',
+  timestamp TEXT,
+  synced_at INTEGER
+)
 ''');
-
+await db.execute('''
+CREATE TABLE fraud_logs (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  tx_id TEXT,
+  reason TEXT,
+  created_at INTEGER
+)
+''');
 }
 
-// ======================
-// AUTH & USER
-// ======================
-
 Future<int> createUser(Map<String, dynamic> user) async {
-
 final dbClient = await database;
-
-// 1. التأكد من أن رقم الهوية فريد محلياً
 final List<Map<String, dynamic>> res = await dbClient.query(
 'users',
 where: 'id_number = ?',
 whereArgs: [user['id_number']],
 );
-
 if (res.isNotEmpty) {
 throw Exception("رقم الهوية هذا مستخدم مسبقاً");
 }
-
-// 2. إدخال البيانات
 return await dbClient.insert(
 'users',
 user,
@@ -121,62 +100,19 @@ conflictAlgorithm: ConflictAlgorithm.abort,
 
 Future<Map<String, dynamic>?> login(String idNumber, String password) async {
 final db = await database;
+final result = await db.query(
+  'users',
+  where: 'id_number = ?',
+  whereArgs: [idNumber],
+);
+if (result.isEmpty) return null;
+final user = result.first;
+final salt = user['salt']?.toString() ?? "";
+final hashedPassword = sha256.convert(utf8.encode(password + salt)).toString();
+if (hashedPassword != user['password']) return null;
+return {'id': user['id'], 'name': user['name']};
+}
 
-final result = await db.query(  
-  'users',  
-  where: 'id_number = ?',  
-  whereArgs: [idNumber],  
-);  
-
-if (result.isEmpty) return null;  
-
-final user = result.first;  
-final salt = user['salt']?.toString() ?? "";  
-
-final hashedPassword = sha256  
-    .convert(utf8.encode(password + salt))  
-    .toString();  
-
-if (hashedPassword != user['password']) {  
-  return null;  
-}  
-
-return {'id': user['id'], 'name': user['name']};}
-
-// ======================
-// SIGNATURE (Offline Logic)
-// ======================
-//String generateSignature({
-//required String txId,
-//required int senderId,
-//required double amount,
-//required int timestamp,
-//}) {
-//final secret = "USER_${senderId}_SECRET_KEY";
-//final data = "$txId|$senderId|$amount|$timestamp|$secret";
-//return //
-//sha256.convert(utf8.encode(data)).toString();
-//}
-
-//bool verifySignature({
-//required String txId,
-//required int senderId,
-//required double amount,
-//required int timestamp,
-//required String signature,
-//}) {
-//final expected = generateSignature(
-//txId: txId,
-//senderId: senderId,
-//amount: amount,
-//timestamp: timestamp,
-//);
-//return expected == signature;
-//}
-
-// ======================
-// CORE TRANSACTIONS
-// ======================
 Future<void> receiveTokens({
 required String txId,
 required int senderId,
@@ -187,84 +123,63 @@ required int timestamp,
 }) async {
 final db = await database;
 bool locked = await SessionManager.isBlocked();
-
-if (locked) {  
-  throw Exception("رصيد غير كافي");  
-}  
-
+if (locked) {
+  throw Exception("🚨 التطبيق مقفل مؤقتاً، لا يمكن إتمام العملية");
+}
 await db.transaction((txn) async {
-
 final existing = await txn.query(
 'transactions',
 where: 'tx_id = ?',
 whereArgs: [txId],
 );
-
 if (existing.isNotEmpty) {
 throw Exception("⚠️ مستخدم مسبقاً");
 }
-
-final now = DateTime.now().millisecondsSinceEpoch;  
-  if (now - timestamp > 5 * 60 * 1000) {  
-    throw Exception("⛔ انتهت صلاحية QR");  
-  }  
+final now = DateTime.now().millisecondsSinceEpoch;
+if (now - timestamp > 5 * 60 * 1000) {
+  throw Exception("⛔ انتهت صلاحية QR");
+}
 final data = CryptoHelper.buildRawData(
   txId: txId,
   senderId: senderId,
   amountStr: amount.toString(),
   timestamp: timestamp,
 );
-
-final valid = CryptoHelper.verify(
-  data,
-  signature,
-  senderId,
+final valid = CryptoHelper.verify(data, signature, senderId);
+if (!valid) {
+  throw Exception("❌ QR غير صالح");
+}
+final sender = await txn.query(
+  'users',
+  where: 'id = ?',
+  whereArgs: [senderId],
+  columns: ['balance'],
 );
-
- 
-
-  if (!valid) {  
-    throw Exception("❌ QR غير صالح");  
-  }  
-
-  final sender = await txn.query(  
-    'users',  
-    where: 'id = ?',  
-    whereArgs: [senderId],  
-    columns: ['balance'],  
-  );  
-
-  final balance = (sender.first['balance'] as num).toDouble();  
-
-  if (balance < amount) {  
-    throw Exception("❌ رصيد غير كافي");  
-  }  
-
-
-  await txn.rawUpdate(  
-    'UPDATE users SET balance = balance - ? WHERE id = ?',  
-    [amount, senderId],  
-  );  
-
-  await txn.rawUpdate(  
-    'UPDATE users SET balance = balance + ? WHERE id = ?',  
-    [amount, receiverId],  
-  );  
-
-  await txn.insert('transactions', {  
-    'tx_id': txId,  
-    'sender_id': senderId,  
-    'receiver_id': receiverId,  
-    'amount': amount,  
-    'type': 'transfer',  
-    'status': 'completed',  
-    'signature': signature,  
-    'created_at': now,  
-    'sync_status': 'pending',  
-    'synced_at': null,  
-  });  
+final balance = (sender.first['balance'] as num).toDouble();
+if (balance < amount) {
+  throw Exception("❌ رصيد غير كافي");
+}
+await txn.rawUpdate(
+  'UPDATE users SET balance = balance - ? WHERE id = ?',
+  [amount, senderId],
+);
+await txn.rawUpdate(
+  'UPDATE users SET balance = balance + ? WHERE id = ?',
+  [amount, receiverId],
+);
+await txn.insert('transactions', {
+  'tx_id': txId,
+  'sender_id': senderId,
+  'receiver_id': receiverId,
+  'amount': amount,
+  'type': 'transfer',
+  'status': 'completed',
+  'signature': signature,
+  'created_at': now,
+  'sync_status': 'pending',
+  'synced_at': null,
 });
-
+});
 }
 
 Future<double> getUserBalance(int userId) async {
@@ -276,7 +191,6 @@ whereArgs: [userId],
 columns: ['balance'],
 );
 if (result.isNotEmpty) {
-// نأخذ القيمة ونحولها لـ double بأمان
 return (result.first['balance'] as num).toDouble();
 }
 return 100.0;
@@ -284,169 +198,123 @@ return 100.0;
 
 Future<List<Map<String, dynamic>>> getRecentTransactions(int userId) async {
 final db = await database;
-
-return await db.query(  
-  'transactions',  
-  where: 'sender_id = ? OR receiver_id = ?',  
-  whereArgs: [userId, userId],  
-  orderBy: 'created_at DESC',  
-  limit: 20,  
+return await db.query(
+  'transactions',
+  where: 'sender_id = ? OR receiver_id = ?',
+  whereArgs: [userId, userId],
+  orderBy: 'created_at DESC',
+  limit: 20,
 );
-
 }
 
 Future<List<Map<String, dynamic>>> getUserTransactions(int userId) async {
 final db = await database;
-
-return await db.rawQuery(  
-  '''  
-SELECT t.*,  
-sender.name AS sender_name,  
-receiver.name AS receiver_name  
-FROM transactions t  
-LEFT JOIN users sender ON sender.id = t.sender_id  
-LEFT JOIN users receiver ON receiver.id = t.receiver_id  
-WHERE t.sender_id = ? OR t.receiver_id = ?  
+return await db.rawQuery('''
+SELECT t.*,
+sender.name AS sender_name,
+receiver.name AS receiver_name
+FROM transactions t
+LEFT JOIN users sender ON sender.id = t.sender_id
+LEFT JOIN users receiver ON receiver.id = t.receiver_id
+WHERE t.sender_id = ? OR t.receiver_id = ?
 ORDER BY t.created_at DESC
-
-''',
-[userId, userId],
-);
+''', [userId, userId]);
 }
+
 Future<bool> isTransactionExists(String txId) async {
 final db = await database;
-
-final result = await db.query(  
-  'transactions',  
-  where: 'tx_id = ?',  
-  whereArgs: [txId],  
-);  
-
-
-// إذا كانت النتيجة ليست فارغة، فهذا يعني أن العملية مسجلة مسبقاً  
+final result = await db.query(
+  'transactions',
+  where: 'tx_id = ?',
+  whereArgs: [txId],
+);
 return result.isNotEmpty;
-
 }
 
 Future<String> getUserName(int userId) async {
 final db = await database;
-
-final res = await db.query(  
-  'users',  
-  where: 'id = ?',  
-  whereArgs: [userId],  
-  columns: ['name'],  
-);  
-
-if (res.isNotEmpty) {  
-  return res.first['name'] as String;  
-}  
-
+final res = await db.query(
+  'users',
+  where: 'id = ?',
+  whereArgs: [userId],
+  columns: ['name'],
+);
+if (res.isNotEmpty) return res.first['name'] as String;
 return "مستخدم";
-
 }
 
 Future<List<Map<String, dynamic>>> getPendingTransactions() async {
 final db = await instance.database;
-
-return await db.query(  
-  'transactions',  
-  where: 'sync_status = ? OR sync_status = ?',  
-  whereArgs: ['pending', 'failed'],  
+return await db.query(
+  'transactions',
+  where: 'sync_status = ? OR sync_status = ?',
+  whereArgs: ['pending', 'failed'],
 );
-
 }
 
 Future<int> countRecentTransactions(int userId) async {
 final db = await database;
-
-int fiveMinutesAgo = DateTime.now()  
-     .subtract(const Duration(minutes: 5))  
-         .millisecondsSinceEpoch;  
-
-final List<Map<String, dynamic>> result = await db.rawQuery(  
-  '''  
-SELECT COUNT(*) as total   
-FROM transactions   
-WHERE (sender_id = ? OR receiver_id = ?)  
- AND created_at > ?  
-   ''',  
- [userId, userId, fiveMinutesAgo]);  
-
-  return Sqflite.firstIntValue(result) ?? 0;  
-     }  
-
-// 3. استخراج العدد من النتيجة
+int fiveMinutesAgo = DateTime.now()
+    .subtract(const Duration(minutes: 5))
+    .millisecondsSinceEpoch;
+final List<Map<String, dynamic>> result = await db.rawQuery('''
+SELECT COUNT(*) as total
+FROM transactions
+WHERE (sender_id = ? OR receiver_id = ?)
+AND created_at > ?
+''', [userId, userId, fiveMinutesAgo]);
+return Sqflite.firstIntValue(result) ?? 0;
+}
 
 Future<void> markAsSynced(String txId) async {
 final db = await database;
-
-await db.update(  
-  'transactions',  
-  {  
-    'sync_status': 'synced',  
-    'synced_at': DateTime.now().millisecondsSinceEpoch,  
-  },  
-  where: 'tx_id = ?',  
-  whereArgs: [txId],  
+await db.update(
+  'transactions',
+  {
+    'sync_status': 'synced',
+    'synced_at': DateTime.now().millisecondsSinceEpoch,
+  },
+  where: 'tx_id = ?',
+  whereArgs: [txId],
 );
-
 }
 
 Future<void> markAsFailed(String txId) async {
 final db = await database;
-
-await db.update(  
-  'transactions',  
-  {'sync_status': 'failed'},  
-  where: 'tx_id = ?',  
-  whereArgs: [txId],  
+await db.update(
+  'transactions',
+  {'sync_status': 'failed'},
+  where: 'tx_id = ?',
+  whereArgs: [txId],
 );
-
 }
 
 Future<void> logFraud(String txId, String reason) async {
 final db = await database;
-
-await db.insert('fraud_logs', {  
-  'tx_id': txId,  
-  'reason': reason,  
-  'created_at': DateTime.now().millisecondsSinceEpoch,  
+await db.insert('fraud_logs', {
+  'tx_id': txId,
+  'reason': reason,
+  'created_at': DateTime.now().millisecondsSinceEpoch,
 });
-
 }
 
-// دالة شحن الرصيد في قاعدة البيانات
 Future<int> topUpBalance(int userId, double amount) async {
 final db = await instance.database;
-
-// نقوم بجلب الرصيد الحالي أولاً ثم إضافة المبلغ الجديد  
-return await db.rawUpdate(  
-  '''  
-UPDATE users   
-SET balance = balance + ?   
+return await db.rawUpdate('''
+UPDATE users
+SET balance = balance + ?
 WHERE id = ?
-
-''',
-[amount, userId],
-);
+''', [amount, userId]);
 }
 
-// 💰 تحديث رصيد المستخدم (خصم أو إضافة)
-Future<void> updateUserBalance(int userId, double amount) async {
+Future<void> updateUserBalance(int userId, double newBalance) async {
 final db = await instance.database;
-
-await db.rawUpdate(  
-  '''  
-  UPDATE users   
-  SET balance = balance + ?   
-     sync_status = 'pending'   
-  WHERE id = ?  
-''',  
-  [amount, userId],  
-);  
-
-debugPrint(" تم تحديث الرصيد للمستخدم $userId بمقدار $amount");
-
+await db.rawUpdate('''
+  UPDATE users
+  SET balance = ?,
+      sync_status = 'pending'
+  WHERE id = ?
+''', [newBalance, userId]);
+debugPrint("تم تحديث الرصيد للمستخدم $userId إلى $newBalance");
 }
 }
